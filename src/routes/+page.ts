216b8d2587848dd050054e52eb750fc49ebc34ba
thomasmichaelkane <thomas.kane.ucl@gmail.com>
2025-07-25 14:27:30 +0100

    fixed build issue in +page.ts due to uncomitted json

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,10 @@
 import type { PageLoad } from './$types';
 import infoData from '$lib/data/info.json';
 import projectsData from '$lib/data/projects.json';
-import publicationsData from '$lib/data/publications.json';
 
 export const load: PageLoad = async () => {
   return {
     info: infoData,
     projects: projectsData.projects,
-    publications: publicationsData.publications
   };
 };
